3c422b9adbf96d416da989e86a29949155344fce
Chris Fordham <chris.fordham@rightscale.com>
2012-08-01 22:37:01 +1000

    Add collectd to update-metadata.sh.

--- update-metadata.sh
+++ update-metadata.sh
@@ -6,3 +6,4 @@ knife cookbook metadata ntp -o cookbooks/
 knife cookbook metadata postfix -o cookbooks/
 knife cookbook metadata python -o cookbooks/
 knife cookbook metadata sudo -o cookbooks/
+knife cookbook metadata collectd -o cookbooks/
