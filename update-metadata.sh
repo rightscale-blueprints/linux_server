d85a6fcf1fe4b716bfc8e7b3272a70267fb70d1d
Chris Fordham <chris.fordham@rightscale.com>
2012-08-02 18:33:06 +1000

    Add update of apach2 metadata to update-metadata.sh.

--- update-metadata.sh
+++ update-metadata.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
 # generate metadata.json for upstream opscode cookbooks that have no metadata.json
+knife cookbook metadata apache2 -o cookbooks/
 knife cookbook metadata cron -o cookbooks/
 knife cookbook metadata ntp -o cookbooks/
 knife cookbook metadata postfix -o cookbooks/
