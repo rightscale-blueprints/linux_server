8d8d911415bb564bb412d74d0a6f06931491a345
Chris Fordham <chris.fordham@rightscale.com>
2012-08-25 10:54:48 +1000

    Uncomment metadata.json generation for collectd cookbook.

--- update-metadata.sh
+++ update-metadata.sh
@@ -7,4 +7,4 @@ knife cookbook metadata ntp -o cookbooks/
 knife cookbook metadata postfix -o cookbooks/
 knife cookbook metadata python -o cookbooks/
 knife cookbook metadata sudo -o cookbooks/
-#knife cookbook metadata collectd -o cookbooks/
+knife cookbook metadata collectd -o cookbooks/
