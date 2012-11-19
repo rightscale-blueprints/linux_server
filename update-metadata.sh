a3f236e53550feb5f5d738a80ddf587a89df1afe
Chris Fordham <chris@xhost.com.au>
2012-11-19 17:13:40 +1100

    Add all cookbooks to update-metadata.sh.

--- update-metadata.sh
+++ update-metadata.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-# generate metadata.json for upstream opscode cookbooks that have no metadata.json
+# generate metadata.json for all upstream opscode cookbooks (metadata.json should not exist in development repositories)
 knife cookbook metadata apache2 -o cookbooks/
 knife cookbook metadata build-essential -o cookbooks/
 knife cookbook metadata collectd -o cookbooks/
