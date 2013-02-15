b46c53ca5436a4761c65636f14a600130d6f65ed
Chris Fordham <chris.fordham@rightscale.com>
2013-02-15 14:22:07 +1100

    Re-generate metadata.json files w/ removal of .gitignore for collectd* cookbooks; updating update-metadata.sh.

--- update-metadata.sh
+++ update-metadata.sh
@@ -3,8 +3,8 @@
 # generate metadata.json for all upstream opscode cookbooks (metadata.json should not exist in development repositories)
 knife cookbook metadata apache2 -o cookbooks/
 knife cookbook metadata build-essential -o cookbooks/
-knife cookbook metadata collectd -o cookbooks/
-knife cookbook metadata collectd_plugins -o cookbooks/
+knife cookbook metadata collectd -o cookbooks/ && [ -e ./cookbooks/collectd/.gitignore ] && rm ./cookbooks/collectd/.gitignore
+knife cookbook metadata collectd_plugins -o cookbooks/ && [ -e ./cookbooks/collectd/.gitignore ] && rm ./cookbooks/collectd_plugins/.gitignore
 knife cookbook metadata cron -o cookbooks/
 knife cookbook metadata ntp -o cookbooks/
 knife cookbook metadata postfix -o cookbooks/
